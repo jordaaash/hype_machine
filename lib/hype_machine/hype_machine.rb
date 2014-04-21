fee47cfc7bd4cb842b838854f19f2e6195e65dc5
Jordan Sexton <jordan@jordansexton.com>
2014-04-20 22:05:29 -0500

    Refactored option parsing and ARGV values from HypeMachine class to bin/hypem

--- lib/hype_machine/hype_machine.rb
+++ lib/hype_machine/hype_machine.rb
@@ -1,12 +1,23 @@
 require 'nokogiri'
 require 'json'
 require 'net/http'
-require 'optparse'
 require 'taglib'
 
 module HypeMachine
   class HypeMachine
     DEFAULTS = {
+      :options       => {
+        :start     => 1,
+        :finish    => nil,
+        :wait      => 0,
+        :proxy     => nil,
+        :port      => nil,
+        :tor       => false,
+        :overwrite => false,
+        :quiet     => false,
+        :strict    => false,
+        :demo      => false
+      },
       :headers       => {
         'Accept'              => 'text/html, application/xhtml+xml, application/xml;q=0.9,*/*;q=0.8',
         'Accept-Charset'      => 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
@@ -28,148 +39,46 @@ module HypeMachine
       }
     }
 
-    attr_accessor :options, :path, :directory, :url, :tab, :completed, :failed,
-                  :skipped, :total, :headers
+    attr_accessor :path, :directory, :options, :url, :headers,
+                  :completed, :failed, :skipped, :total
 
-    def initialize (path = ARGV[1], directory = ARGV[2])
-      tab     = '    '
-      options = {}
+    def initialize (path, directory = nil, options = {})
+      path = path.strip
+      raise 'Invalid path' if path.nil? || path.empty?
 
-      OptionParser.new { |option|
-        option.banner = strip_heredoc(<<-BANNER)
+      directory = File.expand_path(directory || URI.unescape(path).gsub(DEFAULTS[:filename][:invalid_characters], '_'))
 
-          Command line tool for downloading tracks from HypeMachine.
+      options = DEFAULTS[:options].merge(options)
 
-          Usage: hypem [options?] path directory?
+      if options[:tor]
+        options[:proxy] = DEFAULTS[:tor][:proxy]
+        options[:port]  = DEFAULTS[:tor][:port]
+      end
 
-          path: Required.
-          #{tab}HypeMachine user, blog, section, artist, etc.
-          #{tab}Paths with spaces must be quoted.
-          #{tab}(e.g., anthony, blog/earmilk/11067, popular, "artist/Wiz Khalifa")
-
-          directory: Optional.
-          #{tab}Absolute or relative path to a new or existing directory to download to.
-          #{tab}Paths with spaces must be quoted.
-          #{tab}(e.g. /usr/music, ../music/hypem, "C:\\Users\\Guest\\My Documents\\Music")
-          #{tab}If not provided, a directory will be chosen based on the path given and relative to the current directory.
-
-          options: Optional.
-        BANNER
-
-        option.on('-h', '--help', 'Display this help screen') do
-          puts option
-          exit
-        end
-
-        option.on('-v', '--version', 'Display the script version') do
-          puts ::HypeMachine.version
-          exit
-        end
-
-        options[:start] = 1
-        option.on('-s', '--start START', 'Download tracks starting from page #START') do |start|
-          options[:start] = start.to_i
-        end
-
-        options[:finish] = nil
-        option.on('-f', '--finish FINISH', 'Download tracks ending on page #FINISH') do |finish|
-          options[:finish] = finish.to_i
-        end
-
-        options[:wait] = 0
-        option.on('-w', '--wait WAIT', 'Wait #WAIT seconds before downloading each track') do |wait|
-          options[:wait] = wait.to_i
-        end
-
-        option.on('-r', '--tor', "Use Tor as a proxy (--host=#{DEFAULTS[:tor][:proxy]} --port=#{DEFAULTS[:tor][:port]})") do
-          options[:proxy] = DEFAULTS[:tor][:proxy];
-          options[:port] = DEFAULTS[:tor][:port];
-        end
-
-        options[:proxy] = nil
-        option.on('-x', '--proxy PROXY', 'Route through proxy host PROXY') do |proxy|
-          options[:proxy] = proxy.to_s
-        end
-
-        options[:port] = nil
-        option.on('-p', '--port PORT', 'Route through proxy port #PORT') do |port|
-          options[:port] = port.to_i
-        end
-
-        options[:overwrite] = false
-        option.on('-o', '--overwrite', 'Turn on overwrite mode to overwrite existing files') do
-          options[:overwrite] = true
-        end
-
-        options[:quiet] = false
-        option.on('-q', '--quiet', 'Turn on quiet mode to hide console output') do
-          options[:quiet] = true
-        end
-
-        options[:strict] = false
-        option.on('-t', '--strict', 'Turn on strict mode to fail on track errors') do
-          options[:strict] = true
-        end
-
-        options[:demo] = false
-        option.on('-d', '--demo', 'Turn on demo mode to skip downloading') do
-          options[:demo] = true
-        end
-      }.parse!
-      options[:finish] = options[:start] if options[:finish].nil?
+      options[:finish]  ||= options[:start]
+      options[:headers] ||= DEFAULTS[:headers].dup
 
       raise "Invalid start #{options[:start]}" unless options[:start] >= 1
       raise "Invalid finish #{options[:finish]}" unless options[:finish] >= options[:start]
       raise "Invalid wait #{options[:wait]}" unless options[:wait] >= 0
 
-      puts "Wait: #{options[:wait] > 0 ? options[:wait] : 'NO'}"
-      puts "Proxy: #{options[:proxy] ? "#{options[:proxy]}:#{options[:port]}" : 'NO'}"
-      puts "Overwrite: #{options[:overwrite] ? 'YES' : 'NO'}"
-      puts "Quiet: #{options[:quiet] ? 'YES' : 'NO'}"
-      puts "Strict: #{options[:strict] ? 'YES' : 'NO'}"
-      puts "Demo: #{options[:demo] ? 'YES' : 'NO'}"
-      puts ''
-
-      unless path
-        puts strip_heredoc(<<-PROMPT)
-          Enter a path to download from -- a HypeMachine user, blog, section, artist, etc.
-          (e.g., anthony, blog/earmilk/11067, popular, "artist/Wiz Khalifa")
-          Paths with spaces must be quoted.
-
-        PROMPT
-        path = gets
-        puts ''
-      end
-      path = path.strip
-      raise 'Invalid path' if path.nil? || path.empty?
-
-      directory = File.expand_path(directory || URI.unescape(path).gsub(DEFAULTS[:filename][:invalid_characters], '_'))
-      Dir.mkdir(directory) unless File.directory?(directory)
-
-      url       = "http://hypem.com/#{URI.escape(URI.unescape(path))}"
-      completed = 0
-      failed    = 0
-      skipped   = 0
-      total     = 0
-      headers   = DEFAULTS[:headers].dup
-
       frame_factory                       = TagLib::ID3v2::FrameFactory.instance
       frame_factory.default_text_encoding = TagLib::String::UTF8
 
-      self.options   = options
       self.path      = path
       self.directory = directory
-      self.url       = url
-      self.tab       = tab
-      self.completed = completed
-      self.failed    = failed
-      self.skipped   = skipped
-      self.total     = total
-      self.headers   = headers
+      self.options   = options
+      self.headers   = options[:headers]
+      self.url       = "http://hypem.com/#{URI.escape(URI.unescape(path))}"
+      self.completed = 0
+      self.failed    = 0
+      self.skipped   = 0
+      self.total     = 0
     end
 
     def run
       puts "Downloading from #{url}/#{options[:start]} to #{url}/#{options[:finish]} into #{directory}", '' unless options[:quiet]
+      Dir.mkdir(directory) unless File.directory?(directory)
       (options[:start]..options[:finish]).each { |page| scan(page) }
       puts "Finished downloading tracks from #{url}/#{options[:start]} to #{url}/#{options[:finish]}", "#{total} total, #{completed} completed, #{skipped} skipped, #{failed} failed", 'Enjoy!', ''
     end
@@ -183,7 +92,7 @@ module HypeMachine
 
       if response.is_a?(Net::HTTPRedirection) && redirects > 0
         url = response['Location']
-        puts "#{tab}#{tab}Redirecting request to #{url}" unless options[:quiet]
+        puts "\t\tRedirecting request to #{url}" unless options[:quiet]
         response = request(url, headers, redirects - 1)
       end
 
@@ -194,7 +103,7 @@ module HypeMachine
     def scan (page)
       page_url           = "#{url}/#{page}"
       headers['Referer'] = page_url
-      puts "#{tab}Scanning for tracks at #{page_url}" unless options[:quiet]
+      puts "\tScanning for tracks at #{page_url}" unless options[:quiet]
       response          = request("#{page_url}?ax=1", headers)
       headers['Cookie'] = response['Set-Cookie'] if response['Set-Cookie']
 
@@ -208,9 +117,9 @@ module HypeMachine
 
       self.total += total
 
-      puts "#{tab}Found #{total} tracks at #{page_url}", '' unless options[:quiet]
+      puts "\tFound #{total} tracks at #{page_url}", '' unless options[:quiet]
       tracks.each do |track|
-        success = download(track, page_url)
+        success = download(document, track, page_url)
         if success.nil?
           failed      += 1
           self.failed += 1
@@ -223,10 +132,10 @@ module HypeMachine
         end
       end
 
-      puts "#{tab}Finished downloading tracks from #{page_url}", "#{tab}#{total} total, #{completed} completed, #{skipped} skipped, #{failed} failed", '' unless options[:quiet]
+      puts "\tFinished downloading tracks from #{page_url}", "\t#{total} total, #{completed} completed, #{skipped} skipped, #{failed} failed", '' unless options[:quiet]
     end
 
-    def download (track, page_url)
+    def download (document, track, page_url)
       success   = nil
       id        = track['id']
       key       = track['key']
@@ -253,22 +162,22 @@ module HypeMachine
       end
 
       if options[:demo]
-        puts "#{tab}#{tab}Skipping track #{filename} in demo mode", '' unless options[:quiet]
+        puts "\t\tSkipping track #{filename} in demo mode", '' unless options[:quiet]
         success = false
       else
         begin
           if File.exists?(filepath) && !options[:overwrite]
-            puts "#{tab}#{tab}Skipping existing track #{filename}", '' unless options[:quiet]
+            puts "\t\tSkipping existing track #{filename}", '' unless options[:quiet]
             success = false
           else
             if options[:wait] > 0
-              puts "#{tab}#{tab}Waiting #{options[:wait]} seconds", '' unless options[:quiet]
+              puts "\t\tWaiting #{options[:wait]} seconds", '' unless options[:quiet]
               sleep(options[:wait])
             end
 
-            puts "#{tab}#{tab}Starting track #{filename}" unless options[:quiet]
+            puts "\t\tStarting track #{filename}" unless options[:quiet]
             source_url = "http://hypem.com/serve/source/#{id}/#{key}"
-            puts "#{tab}#{tab}Getting source from #{source_url}" unless options[:quiet]
+            puts "\t\tGetting source from #{source_url}" unless options[:quiet]
             response = request(source_url, headers)
 
             data = JSON.parse(response.body)
@@ -276,35 +185,36 @@ module HypeMachine
 
             file_url = data['url']
 
-            puts "#{tab}#{tab}Downloading from #{file_url}" unless options[:quiet]
+            puts "\t\tDownloading from #{file_url}" unless options[:quiet]
             begin
               response = request(file_url, headers)
             rescue StandardError => error
               source_url += "?retry=1"
-              puts "#{tab}#{tab}Retry getting source from #{source_url}" unless options[:quiet]
+              puts "\t\tRetry getting source from #{source_url}" unless options[:quiet]
               response = request(source_url, headers)
-              data     = JSON.parse(response.body)
+
+              data = JSON.parse(response.body)
               raise 'Invalid track ID' unless data['itemid'] == id
               file_url = data['url']
-              puts "#{tab}#{tab}Retry downloading from #{file_url}" unless options[:quiet]
+
+              puts "\t\tRetry downloading from #{file_url}" unless options[:quiet]
               response = request(file_url, headers)
             end
 
-            puts "#{tab}#{tab}Writing data to file #{filepath}" unless options[:quiet]
-            File.open(filepath, 'wb') do |file|
-              file.write(response.body)
+            puts "\t\tWriting data to file #{filepath}" unless options[:quiet]
+            File.open(filepath, 'wb') { |file| file.write(response.body) }
+
+            if File.exists?("#{filepath}.log")
+              puts "\t\tDeleting log file #{filepath}.log" unless options[:quiet]
+              File.delete("#{filepath}.log")
             end
 
-            File.delete("#{filepath}.log") if File.exists?("#{filepath}.log")
-
-            puts "#{tab}#{tab}Updating ID3 metadata of file #{filepath}" unless options[:quiet]
-
             TagLib::MPEG::File.open(filepath) do |file|
+              puts "\t\tUpdating ID3 metadata of file #{filepath}" unless options[:quiet]
               tag(file, artist, title, id, thumb_url)
             end
 
-            puts "#{tab}#{tab}Finished downloading track #{filename}", '' unless options[:quiet]
-
+            puts "\t\tFinished downloading track #{filename}", '' unless options[:quiet]
             success = true
           end
         rescue StandardError => error
@@ -320,7 +230,7 @@ module HypeMachine
                 Error  : #{message}
               LOG
             end
-            puts "#{tab}#{tab}Exception logged to #{filename}.log: #{message}", '' unless options[:quiet]
+            puts "\t\tException logged to #{filename}.log: #{message}", '' unless options[:quiet]
           end
         end
       end
